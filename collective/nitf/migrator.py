d216f4379d6811a9f937969c8e4119bdcddce8e8
saibatizoku <saibatizoku@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-06-29 18:15:57 +0000

    migrator imports all fields from news items into nitf articles
    git-svn-id: https://svn.plone.org/svn/collective/collective.nitf/trunk@241655 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- collective/nitf/migrator.py
+++ collective/nitf/migrator.py
@@ -1,11 +1,22 @@
 # encoding: utf-8
 import transaction
+from zope.component import queryMultiAdapter
+from zope.event import notify
 from zope.interface import classProvides
 from zope.interface import implements
+from zope.lifecycleevent import ObjectAddedEvent
+from zope.lifecycleevent import ObjectModifiedEvent
+from zope.schema import getFieldsInOrder
 
 from Products.CMFPlone.utils import getToolByName
+from plone.app.textfield.value import RichTextValue
+from plone.dexterity.utils import iterSchemata
+from plone.uuid.interfaces import IMutableUUID
+from z3c.form.interfaces import IValue
 
 from collective.transmogrifier.interfaces import ISection, ISectionBlueprint
+
+_marker = object()
 
 
 class NewsReader(object):
@@ -39,11 +50,69 @@ class NITFTransform(object):
             parent = objct.__parent__
             o_id = item['id']
             n_id = o_id + '-old'
-            parent[n_id] = objct
-            del parent[o_id]
-            parent.invokeFactory('collective.nitf.content', id=o_id, title=item["Title"])
-            del parent[n_id]
-            item = parent[o_id]
-            item.reindexObject()
-            yield item
+            parent.manage_renameObject(o_id, n_id)
+            news_obj = parent[n_id]
+            #parent[n_id] = objct
+            #del parent[o_id]
+            parent.invokeFactory('collective.nitf.content', id=o_id,
+                                 )
+            nitf_obj = parent[o_id]
+            #get all fields for this obj
+            for schemata in iterSchemata(nitf_obj):
+                for name, field in getFieldsInOrder(schemata):
+                    # FIXME creators field isn't setting at all.
+                    #       problem with dexterity, apparently
+                    #if name is 'creators':
+                    #    continue
+                    #setting value from the blueprint cue
+                    if name == 'creators':
+                        value = news_obj.Creators()
+                        creator_list = []
+                        for creator in value:
+                            creator_list.append(unicode(creator))
 
+                        value = tuple(creator_list)
+                    else:
+                        value = news_obj.get(name, _marker)
+                    if value is _marker:
+                        # No value is given from the pipeline,
+                        # so we try to set the default value
+                        # otherwise we set the missing value
+                        default = queryMultiAdapter((
+                                nitf_obj,
+                                nitf_obj.REQUEST, # request
+                                None, # form
+                                field,
+                                None, # Widget
+                                ), IValue, name='default')
+                        if default is not None:
+                            default = default.get()
+                        if default is None:
+                            default = getattr(field, 'default', None)
+                        if default is None:
+                            try:
+                                default = field.missing_value
+                            except AttributeError:
+                                pass
+                        value = default
+                    field.set(field.interface(nitf_obj), value)
+            nitf_obj.setTitle(u'%s' % news_obj.title)
+            nitf_obj.setDescription(u'%s' % news_obj.Description())
+            nitf_obj.setCreators(news_obj.creators)
+            nitf_obj.body = RichTextValue(news_obj.getText(), 'text/html', 
+                                          'text/x-html-safe')
+            if news_obj.getImage():
+                file_id = '.'.join(['image',
+                                   news_obj.getImage().getContentType()[-3:]])
+                # FIXME debugging lines
+                #print file_id
+                nitf_obj.invokeFactory('Image', id=file_id,
+                                        title=news_obj.getImageCaption(),
+                                        image=news_obj.getRawImage())
+                notify(ObjectAddedEvent(nitf_obj))
+            notify(ObjectModifiedEvent(nitf_obj))
+            # FIXME debugging lines
+            #print nitf_obj.Type(), nitf_obj.Title(), nitf_obj.Description(),
+            #        nitf_obj.creators, nitf_obj.body.raw, nitf_obj.body.output
+            del parent[n_id]
+            yield item
