1a74e70fc705b657f4001c3bb4ef14f0807fe95c
hvelarde <hector.velarde@gmail.com>
2011-08-04 16:01:21 -0500

    added file to update translations

---
+++ rebuild_i18n.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+I18NDOMAIN="collective.nitf"
+
+# Synchronise the templates and scripts with the .pot.
+# All on one line normally:
+bin/i18ndude rebuild-pot --pot src/collective/nitf/locales/${I18NDOMAIN}.pot \
+    --create ${I18NDOMAIN} \
+   .
+
+# Synchronise the resulting .pot with all .po files
+for po in src/collective/nitf/locales/*/LC_MESSAGES/${I18NDOMAIN}.po; do
+    bin/i18ndude sync --pot src/collective/nitf/locales/${I18NDOMAIN}.pot $po
+done
