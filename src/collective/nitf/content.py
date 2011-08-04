03fc404eb3bfaf5caaebdb57db192db5321c3b84
Joaquín Rosales <globojorro@gmail.com>
2011-08-04 15:19:01 -0500

    Media sorter demo; Media uploader demo in progess

--- src/collective/nitf/content.py
+++ src/collective/nitf/content.py
@@ -6,10 +6,14 @@ import unicodedata
 from five import grok
 from zope import schema
 from zope.component import getUtility
+from zope.component import getMultiAdapter
 from zope.interface import Interface
+from zope.interface import alsoProvides
 from zope.schema.interfaces import IVocabularyFactory
 from zope.schema.vocabulary import SimpleVocabulary
 
+from Products.ATContentTypes.interfaces import IImageContent
+from Products.CMFDefault.formlib.schema import SchemaAdapterBase
 from Products.CMFPlone.utils import getToolByName
 
 from plone.app.layout.viewlets.interfaces import IAboveContentBody
@@ -65,18 +69,21 @@ class INITF(form.Schema):
             # nitf/head/tobject/tobject.property/@tobject.property.type
             title=_(u'News Type'),
             vocabulary=config.NEWS_TYPES,
+            required=False,
         )
 
     section = schema.Choice(
             # nitf/head/pubdata/@position.section
             title=_(u'Section'),
             vocabulary=u'collective.nitf.Sections',
+            required=False,
         )
 
     urgency = schema.Choice(
             # nitf/head/docdata/urgency/@ed-urg
             title=_(u'Urgency'),
             vocabulary=config.URGENCIES,
+            required=False,
         )
 
     form.order_after(location='IRelatedItems.relatedItems')
@@ -90,6 +97,14 @@ class INITF(form.Schema):
             required=False,
         )
 
+    def getImage(self):
+        img = None
+        if len(self.context.objectIds()):
+            return self.context[self.context.objectIds()[0]]
+        return
+    
+    def image_thumb(self):
+        return self.getImage()
 
 @form.default_value(field=INITF['kind'])
 def kind_default_value(data):
@@ -97,6 +112,28 @@ def kind_default_value(data):
     settings = registry.forInterface(INITFSettings)
     return settings.default_kind
 
+
+class ImageContentAdapter(SchemaAdapterBase, grok.Adapter):
+    grok.context(INITF)
+    grok.provides(IImageContent)
+
+    def __init__(self, context):
+        super(ImageContentAdapter, self).__init__(context)
+        self.context = context
+
+    def getImage(self):
+        img = None
+        if len(self.context.objectIds()):
+            return self.context[self.context.objectIds()[0]]
+        return
+
+    def setImage(self):
+        return
+
+    def tag(self):
+        return
+
+alsoProvides(INITF, IImageContent)
 
 class SectionsVocabulary(object):
     """Creates a vocabulary with the sections stored in the registry; the
@@ -275,3 +312,19 @@ class MediaLinksViewlet(grok.Viewlet):
 class Media_Sorter(grok.View):
     grok.context(INITF)
     grok.require('cmf.ModifyPortalContent')
+
+
+class Media_Uploader(grok.View):
+    grok.context(INITF)
+    grok.require('cmf.ModifyPortalContent')
+
+    files = []
+
+    def update(self, uploaderfiles=None):
+        #self.files = self.request.form.items()
+        #if uploaderfiles is not None:
+        #    self.files = uploaderfiles
+        if getattr(self.request, "METHOD", None):
+            if self.request["METHOD"] == "POST":
+                self.files = u"Post"
+                return u"POST"
--- src/collective/nitf/tests/test_migrator.py
+++ src/collective/nitf/tests/test_migrator.py
@@ -5,11 +5,35 @@ import unittest2 as unittest
 from zope.component import createObject
 from zope.component import queryUtility
 
+from plone.app.textfield.value import RichTextValue
 from Products.CMFPlone.utils import getToolByName
 from plone.dexterity.interfaces import IDexterityFTI
 
 from collective.transmogrifier.transmogrifier import Transmogrifier
 from collective.nitf.testing import INTEGRATION_TESTING
+
+
+def populateContainer(self, container, default_type='News Item', limit=4):
+    for n in range(0, limit):
+        n_id = 'n%s' % str(n + 1)
+        container.invokeFactory(default_type,
+                                id=n_id,
+                                title=u'News %s' % str(n + 1),
+                                description=u'Description %s' % str(n + 1),
+                                subject=(u"Category 1", u"Category 2", u"A third category")
+                                )
+        notify(ObjectAddedEvent(container[n_id]))
+        if default_type == 'News Item':
+            container[n_id].setText(u'News %s' % str(n + 1))
+            # Randomly sets images on news items
+            if random.choice([True, False]):
+                container[n_id].setImage(StringIO(zptlogo))
+                container[n_id].setImageCaption(u'Zope LOGO %s' % str(n + 1))
+        if default_type == 'collective.nitf.content':
+            container[n_id].body = RichTextValue(u'<p>News %s</p>' % str(n + 1),
+                                    'text/html', 'text/x-html-safe')
+            #logger.debug(u"Populating: %s %s %s" % (default_type, n_id, container[n_id].objectIds()))
+        container[n_id].reindexObject()
 
 
 class TestNITFIntegration(unittest.TestCase):
