5df4262e978d1a8089c9db9159b1ebe7081ef006
hvelarde <hector.velarde@gmail.com>
2011-08-24 11:15:27 -0300

    refactored pipeline section; removed unused code; references #4

--- src/collective/nitf/migrator.py
+++ src/collective/nitf/migrator.py
@@ -3,8 +3,7 @@
 import pprint
 
 from five import grok
-from zope.component import createObject
-from zope.component import queryUtility
+from plone.app.textfield.value import RichTextValue
 from zope.component import queryMultiAdapter
 from zope.event import notify
 from zope.interface import classProvides
@@ -17,8 +16,6 @@ from Products.ATContentTypes.interfaces import IATNewsItem
 from Products.CMFPlone.interfaces import IPloneSiteRoot
 from Products.CMFPlone.utils import _createObjectByType
 from Products.CMFPlone.utils import getToolByName
-from plone.app.textfield.value import RichTextValue
-from plone.dexterity.interfaces import IDexterityFTI
 
 from collective.transmogrifier.interfaces import ISection, ISectionBlueprint
 from collective.transmogrifier.transmogrifier import Transmogrifier
@@ -91,6 +88,7 @@ class SchemaUpdater(object):
     def __init__(self, transmogrifier, name, options, previous):
         self.previous = previous
         self.context = transmogrifier.context
+        # TODO: implement options to set default values
         self.options = options
 
     def __iter__(self):
@@ -132,14 +130,13 @@ class SchemaUpdater(object):
             obj.setContributors(item['contributors'])
             obj.setRights(item['rights'])
 
-            obj.reindexObject()
             notify(ObjectModifiedEvent(obj))
 
             yield item
 
 
 class ImageMigrator(object):
-    """News Item image field converted to ATImage.
+    """Converts the News Item image field into an ATImage.
     """
     classProvides(ISectionBlueprint)
     implements(ISection)
@@ -147,7 +144,6 @@ class ImageMigrator(object):
     def __init__(self, transmogrifier, name, options, previous):
         self.previous = previous
         self.context = transmogrifier.context
-        self.options = options
 
     def __iter__(self):
         for item in self.previous:
@@ -162,76 +158,17 @@ class ImageMigrator(object):
                 yield item; continue
 
             try:
-                _createObjectByType('Image', obj, 'image')
+                _createObjectByType('Image', obj, 'image', title='image')
             except:
+                # could not create image
                 yield item; continue
 
             image = obj['image']
-            image.setImage(item['image'])
-            image.setTitle('image')
             image.setDescription(item['imageCaption'])
+            image.setImage(item['image'])
 
-            obj.reindexObject()
-            notify(ObjectModifiedEvent(obj))
+            notify(ObjectAddedEvent(image))
 
-            yield item
-
-
-class NITFImageImport(object):
-    # image, imageCaption
-    classProvides(ISectionBlueprint)
-    implements(ISection)
-
-    def __init__(self, transmogrifier, name, options, previous):
-        self.previous = previous
-
-    def __iter__(self):
-        for item in self.previous:
-            objct = item.getObject()
-            parent = objct.__parent__
-            o_id = item['id']
-            n_id = o_id + '-old'
-            parent[n_id] = objct
-            news_obj = parent[n_id]
-            news_obj.reindexObject()
-            #parent[n_id] = objct
-            del parent[o_id]
-            #createContentInContainer(parent, 'collective.nitf.content', id=o_id, checkConstraints=False)
-            #parent.invokeFactory('collective.nitf.content', id=o_id,
-            #                     )
-            fti = queryUtility(IDexterityFTI, name='collective.nitf.content')
-            factory = fti.factory
-            new_object = createObject(factory, id=o_id)
-            #parent[o_id] = new_object
-
-            #nitf_obj = parent[o_id]
-            nitf_obj = new_object
-            self.setFieldsDefaults(nitf_obj, news_obj)
-            #get all fields for this obj
-            nitf_obj.setTitle(u'%s' % news_obj.title)
-            nitf_obj.setDescription(u'%s' % news_obj.Description())
-            nitf_obj.setCreators(news_obj.creators)
-            nitf_obj.setSubject(news_obj.Subject())
-            nitf_obj.body = RichTextValue(news_obj.getText(), 'text/html',
-                                          'text/x-html-safe')
-            parent[o_id] = nitf_obj
-            nitf_obj = parent[o_id]
-            notify(ObjectAddedEvent(parent[o_id]))
-            if news_obj.getImage():
-                file_id = '.'.join(['image',
-                                   news_obj.getImage().getContentType()[-3:]])
-                # FIXME debugging lines
-                #print file_id
-                #_createObjectByType('Image', nitf_obj, id=file_id,
-                nitf_obj.invokeFactory('Image', id=file_id,
-                                        title=news_obj.getImageCaption(),
-                                        image=news_obj.getRawImage())
-            notify(ObjectModifiedEvent(parent[o_id]))
-            parent[o_id].reindexObject()
-            # FIXME debugging lines
-            #print nitf_obj.Type(), nitf_obj.Title(), nitf_obj.Description(),
-            #        nitf_obj.creators, nitf_obj.body.raw, nitf_obj.body.output
-            del parent[n_id]
             yield item
 
 
