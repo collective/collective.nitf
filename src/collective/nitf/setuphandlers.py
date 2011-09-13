0d95b2137deebc3f4c7f9c10b45771122f74cd91
hvelarde <hector.velarde@gmail.com>
2011-09-13 16:41:38 -0300

    pep8/pyflakes; removed unused subscriber

--- src/collective/nitf/setuphandlers.py
+++ src/collective/nitf/setuphandlers.py
@@ -1,41 +1,29 @@
 # -*- coding: utf-8 -*-
 
-from zope import schema
 from zope.component import getUtility
 from zope.app.intid.interfaces import IIntIds
 
 from Products.CMFCore.utils import getToolByName
-from plone.registry import field
-from plone.registry import Record
-from plone.registry.interfaces import IRegistry
-
-from collective.nitf import _
-from collective.nitf import config
-
-DEFAULT_KIND = 'collective.nitf.controlpanel.INITFSettings.default_kind'
-EMBEDLY_KEY = 'collective.nitf.controlpanel.INITFSettings.embedly_key'
-
-def updateControlPanelRegistry(portal):
-    registry = getUtility(IRegistry, context=portal)
 
 
+# TODO: document why we're doing this
 def updateIntIds(portal):
     catalog = getToolByName(portal, 'portal_catalog')
     intids = getUtility(IIntIds)
 
     results = catalog.searchResults()
-    for res in results:
-        nitf = res.getObject()
-        if not intids.queryId(nitf):
-            intids.register(nitf)
+    for brain in results:
+        obj = brain.getObject()
+        if not intids.queryId(obj):
+            intids.register(obj)
+
 
 def setupVarious(context):
-    # We check from our GenericSetup context whether we are running
-    # add-on installation for your product or any other proudct
+    # We check from our GenericSetup context whether we are running add-on
+    # installation for your product or any other product
     if context.readDataFile('collective.nitf.marker.txt') is None:
         # Not your add-on
         return
 
     portal = context.getSite()
-    updateControlPanelRegistry(portal)
     updateIntIds(portal)
