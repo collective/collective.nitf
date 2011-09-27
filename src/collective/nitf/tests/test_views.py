fa3af40912606d958fd3bb8a4390a646112dabd1
hvelarde <hector.velarde@gmail.com>
2011-09-27 19:04:04 -0300

    fixed view name on test

---
+++ src/collective/nitf/tests/test_views.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+
+import unittest2 as unittest
+
+from AccessControl import Unauthorized
+from StringIO import StringIO
+
+from zope.app.file.tests.test_image import zptlogo
+from zope.interface import directlyProvides
+
+from plone.app.testing import TEST_USER_ID
+from plone.app.testing import logout
+from plone.app.testing import setRoles
+
+from collective.nitf.interfaces import INITFBrowserLayer
+from collective.nitf.testing import INTEGRATION_TESTING
+
+
+class ViewTest(unittest.TestCase):
+
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.request = self.layer['request']
+        directlyProvides(self.request, INITFBrowserLayer)
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.portal.invokeFactory('Folder', 'test-folder')
+        setRoles(self.portal, TEST_USER_ID, ['Member'])
+        self.folder = self.portal['test-folder']
+        self.folder.invokeFactory('collective.nitf.content', 'n1')
+        self.n1 = self.folder['n1']
+
+    def test_view(self):
+        view = self.n1.restrictedTraverse('view')
+        view.update()
+        self.assertEquals(view.image(), None)
+        images = view.images()
+        self.assertEquals(len(images), 0)
+        files = view.files()
+        self.assertEquals(len(files), 0)
+        links = view.links()
+        self.assertEquals(len(links), 0)
+        self.n1.invokeFactory('Image', 'foo', image=StringIO(zptlogo))
+        self.assertEquals(view.image()['id'], 'foo')
+        images = view.images()
+        self.assertEquals(len(images), 1)
+
+    def test_gallery(self):
+        self.n1.unrestrictedTraverse('@@gallery')
+
+    def test_nitf(self):
+        self.n1.unrestrictedTraverse('@@nitf')
+
+    def test_organize(self):
+        # view can not be accessed by anonymous users
+        logout()
+        self.assertRaises(Unauthorized,
+                          self.n1.restrictedTraverse,
+                         '@@organize')
+
+    def test_media_uploader(self):
+        # view can not be accessed by anonymous users
+        logout()
+        self.assertRaises(Unauthorized,
+                          self.n1.restrictedTraverse,
+                         '@@media_uploader')
+
+
+class HelperViewTest(unittest.TestCase):
+
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.request = self.layer['request']
+        directlyProvides(self.request, INITFBrowserLayer)
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.portal.invokeFactory('Folder', 'test-folder')
+        self.folder = self.portal['test-folder']
+
+    # TODO: rewrite this test to verify the view is in INITFBrowserLayer
+    def test_folder_summary_view(self):
+        view = self.folder.unrestrictedTraverse('@@folder_summary_view')
+
+
+def test_suite():
+    return unittest.defaultTestLoader.loadTestsFromName(__name__)
