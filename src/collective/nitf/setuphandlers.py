d3abc8358a28876d92a771f6a86f119371e4a856
hvelarde <hector.velarde@gmail.com>
2011-12-20 14:11:54 -0600

    remove unneeded import step; hide uninstall profile at install time using Grok

--- src/collective/nitf/setuphandlers.py
+++ src/collective/nitf/setuphandlers.py
@@ -1,29 +1,16 @@
-# -*- coding: utf-8 -*-
+# -*- coding:utf-8 -*-
 
-from zope.component import getUtility
-from zope.app.intid.interfaces import IIntIds
+from five import grok
 
-from Products.CMFCore.utils import getToolByName
-
-
-# TODO: document why we're doing this
-def updateIntIds(portal):
-    catalog = getToolByName(portal, 'portal_catalog')
-    intids = getUtility(IIntIds)
-
-    results = catalog.searchResults()
-    for brain in results:
-        obj = brain.getObject()
-        if not intids.queryId(obj):
-            intids.register(obj)
+from Products.CMFPlone.interfaces import INonInstallable
 
 
-def setupVarious(context):
-    # We check from our GenericSetup context whether we are running add-on
-    # installation for your product or any other product
-    if context.readDataFile('collective.nitf.marker.txt') is None:
-        # Not your add-on
-        return
+class HiddenProfiles(grok.GlobalUtility):
 
-    portal = context.getSite()
-    updateIntIds(portal)
+    grok.implements(INonInstallable)
+    grok.provides(INonInstallable)
+    grok.name('collective.newsflash')
+
+    def getNonInstallableProfiles(self):
+        profiles = ['collective.newsflash:uninstall', ]
+        return profiles
