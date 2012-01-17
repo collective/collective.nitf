67f3a6e8b69181ffb0ccf1f66d911b78aaee0593
hvelarde <hector.velarde@gmail.com>
2012-01-16 22:32:11 -0600

    refactored uninstall registry tests

--- src/collective/nitf/tests/test_registry.py
+++ src/collective/nitf/tests/test_registry.py
@@ -17,6 +17,8 @@ from collective.nitf import config
 from collective.nitf.controlpanel import INITFSettings
 from collective.nitf.testing import INTEGRATION_TESTING
 
+BASE_REGISTRY = 'collective.nitf.controlpanel.INITFSettings.%s'
+
 
 class RegistryTest(unittest.TestCase):
 
@@ -34,7 +36,7 @@ class RegistryTest(unittest.TestCase):
         view = getMultiAdapter((self.portal, self.portal.REQUEST),
                                name=config.CONTROLPANEL_ID)
         view = view.__of__(self.portal)
-        self.failUnless(view())
+        self.assertTrue(view())
 
     def test_nitf_controlpanel_view_protected(self):
         # Test that the NITF setting control panel view can not be accessed
@@ -48,68 +50,61 @@ class RegistryTest(unittest.TestCase):
     def test_nitf_in_controlpanel(self):
         # Check that there is an NITF entry in the control panel
         self.controlpanel = getToolByName(self.portal, 'portal_controlpanel')
-        self.failUnless('nitf' in [a.getAction(self)['id']
+        self.assertTrue('nitf' in [a.getAction(self)['id']
                             for a in self.controlpanel.listActions()])
 
     def test_record_sections(self):
         # Test that the sections record is in the control panel
         record_sections = self.registry.records[
-            'collective.nitf.controlpanel.INITFSettings.sections']
-        self.failUnless('sections' in INITFSettings)
+            BASE_REGISTRY % 'sections']
+        self.assertTrue('sections' in INITFSettings)
         self.assertEquals(record_sections.value, set([]))
 
     def test_record_default_section(self):
         # Test that the default_section record is in the control panel
         record_default_section = self.registry.records[
-            'collective.nitf.controlpanel.INITFSettings.default_section']
-        self.failUnless('default_section' in INITFSettings)
+            BASE_REGISTRY % 'default_section']
+        self.assertTrue('default_section' in INITFSettings)
         self.assertEquals(record_default_section.value, None)
 
     def test_record_default_genre(self):
         # Test that the default_genre record is in the control panel
         record_default_genre = self.registry.records[
-            'collective.nitf.controlpanel.INITFSettings.default_genre']
-        self.failUnless('default_genre' in INITFSettings)
+            BASE_REGISTRY % 'default_genre']
+        self.assertTrue('default_genre' in INITFSettings)
         self.assertEquals(record_default_genre.value,
                           config.DEFAULT_GENRE)
 
     def test_record_default_urgency(self):
         # Test that the default_urgency record is in the control panel
         record_default_urgency = self.registry.records[
-            'collective.nitf.controlpanel.INITFSettings.default_urgency']
-        self.failUnless('default_urgency' in INITFSettings)
+            BASE_REGISTRY % 'default_urgency']
+        self.assertTrue('default_urgency' in INITFSettings)
         self.assertEquals(record_default_urgency.value,
                           config.DEFAULT_URGENCY)
 
 
-from plone.app.testing import TEST_USER_NAME
-from plone.app.testing import login
-
-
 class RegistryUninstallTest(unittest.TestCase):
-    """ensure registry is properly uninstalled"""
+    """Ensure registry is properly uninstalled"""
 
     layer = INTEGRATION_TESTING
 
     def setUp(self):
         self.portal = self.layer['portal']
         setRoles(self.portal, TEST_USER_ID, ['Manager'])
-        login(self.portal, TEST_USER_NAME)
         self.registry = getUtility(IRegistry)
-        # uninstall the package
-        self.qi = getattr(self.portal, 'portal_quickinstaller')
-        self.qi.uninstallProducts(products=[config.PROJECTNAME])
+        qi = getattr(self.portal, 'portal_quickinstaller')
+        qi.uninstallProducts(products=[config.PROJECTNAME])
 
-    def test_records_uninstalled(self):
-        # Test that the records were removed from the control panel
+    def test_records_removed(self):
         records = [
-            'collective.nitf.controlpanel.INITFSettings.sections',
-            'collective.nitf.controlpanel.INITFSettings.default_section',
-            'collective.nitf.controlpanel.INITFSettings.default_genre',
-            'collective.nitf.controlpanel.INITFSettings.default_urgency',
+            BASE_REGISTRY % 'sections',
+            BASE_REGISTRY % 'default_section',
+            BASE_REGISTRY % 'default_genre',
+            BASE_REGISTRY % 'default_urgency',
             ]
         for r in records:
-            self.failIf(r in self.registry)
+            self.assertFalse(r in self.registry)
 
 
 def test_suite():
