4d3f1787b31f48fdea780d9b1aca3f3ffadbd2e2
Gonzalo Almeida <flecox@gmail.com>
2012-01-19 15:06:27 -0300

    adding arrows and closing not defaulttabs in nitf

--- src/collective/nitf/static/nitf_fixes.js
+++ src/collective/nitf/static/nitf_fixes.js
@@ -7,15 +7,45 @@ function collapseSections() {
       if($(".tab-collapser",$(this).parent()).length === 0) {
         $(".field",$(this).parent()).wrapAll("<span class='tab-collapser'>");
         //hide the tab-collapser
-        $(".tab-collapser" ,$(this).parent()).slideToggle();
+        var that = this;
+        $(".tab-collapser" ,$(this).parent()).slideToggle(function() {
+            $(that).css("background", 
+              "url('++resource++collective.nitf/images/arrows.png') no-repeat \
+              scroll 2px -28px transparent");
+        });
       } else {
         //show the fields again and remove the tab-collapser (unwrap the fields)
         //this is done to avoid some compatibility issues with other plone's javascripts
-        $(".tab-collapser" ,$(this).parent()).slideToggle();
-        $(".tab-collapser" ,$(this).parent()).replaceWith(function() { return $(this).contents(); });
+        var that = this;
+        
+        $(".tab-collapser" ,$(this).parent()).slideToggle(function () {
+          $(that).css("background", 
+            "url('++resource++collective.nitf/images/arrows.png') no-repeat \
+            scroll 2px 10px transparent");
+          $(".tab-collapser" ,$(that).parent()).replaceWith(function() {
+            return $(this).contents(); 
+          });
+        });
       }
     })
     $("fieldset legend").css("cursor", "pointer");
+    $("fieldset legend").each(function(index) {
+      if(index != 0) {
+        if($(".tab-collapser",$(this).parent()).length === 0) {
+          $(".field",$(this).parent()).wrapAll("<span class='tab-collapser'>");
+          $(".tab-collapser",$(this).parent()).css("display", "none");
+          $(this).css("background", 
+            "url('++resource++collective.nitf/images/arrows.png') no-repeat \
+            scroll 2px -28px transparent");
+          $(this).css("padding-left", "20px");
+        }
+      } else {
+          $(this).css("background", 
+            "url('++resource++collective.nitf/images/arrows.png') no-repeat \
+            scroll 2px 10px transparent");
+          $(this).css("padding-left", "20px");
+      }
+    })
   } 
 }
 
