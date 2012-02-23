a0479972eda1df73b1ad38b88e3efed9d47a9945
hvelarde <hector.velarde@gmail.com>
2012-02-23 16:21:11 -0200

    updated i18n script

--- rebuild_i18n.sh
+++ rebuild_i18n.sh
@@ -6,12 +6,24 @@ BASE_DIRECTORY="src/collective/nitf"
 
 # Synchronise the templates and scripts with the .pot.
 i18ndude rebuild-pot --pot ${BASE_DIRECTORY}/locales/${I18NDOMAIN}.pot \
+    --merge ${BASE_DIRECTORY}/locales/manual.pot \
     --create ${I18NDOMAIN} \
     ${BASE_DIRECTORY}
 
 # Synchronise the resulting .pot with all .po files
 for po in ${BASE_DIRECTORY}/locales/*/LC_MESSAGES/${I18NDOMAIN}.po; do
     i18ndude sync --pot ${BASE_DIRECTORY}/locales/${I18NDOMAIN}.pot $po
+done
+
+# Synchronise the templates and scripts with the .pot.
+i18ndude rebuild-pot --pot ${BASE_DIRECTORY}/locales/plone.pot \
+    --create plone \
+    ${BASE_DIRECTORY}/configure.zcml \
+    ${BASE_DIRECTORY}/profiles/default
+
+# Synchronise the plone's pot file (Used for the workflows)
+for po in ${BASE_DIRECTORY}/locales/*/LC_MESSAGES/plone.po; do
+    i18ndude sync --pot ${BASE_DIRECTORY}/locales/plone.pot $po
 done
 
 # Report of errors and suspect untranslated messages
