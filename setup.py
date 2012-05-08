960f68ecf789a4cab11f88230c0e3b4dd4fe47a2
hvelarde <hector.velarde@gmail.com>
2012-05-08 12:07:00 -0300

    we will use released 1.0a1 version of collective.z3cform.widgets instead of development one

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(name='collective.nitf',
         'plone.app.lockingbehavior',
         'plone.app.referenceablebehavior',
         'plone.app.relationfield',
-        'collective.z3cform.widgets',
+        'collective.z3cform.widgets>=1.0a1',
         ],
       extras_require={
         'test': ['plone.app.testing'],
