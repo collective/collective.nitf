8a3458a4dad9745fcf9664df0e903ea2507d4f8b
Gonzalo Almeida <flecox@gmail.com>
2012-05-17 12:40:38 -0300

    changing the name of KeywordWidget to the new name TokenInputWidget

--- src/collective/nitf/__init__.py
+++ src/collective/nitf/__init__.py
@@ -11,6 +11,6 @@ from zope import schema as _schema
 
 _directives_values = ICategorization.queryTaggedValue(TEMP_KEY)
 _directives_values.setdefault(WIDGETS_KEY, {})
-widget = 'collective.z3cform.widgets.keywords_widget.KeywordsFieldWidget'
+widget = 'collective.z3cform.widgets.token_input_widget.TokenInputFieldWidget'
 _directives_values[WIDGETS_KEY]['subjects'] = widget
 _schema.getFields(ICategorization)['subjects'].index_name = 'Subject'
