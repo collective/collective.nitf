ba8da973be7e7b2757c62c55eb13261b373824f0
hvelarde <hector.velarde@gmail.com>
2012-07-11 19:23:09 -0300

    uninstall profile should not be applied on reinstall

--- src/collective/nitf/Extensions/Install.py
+++ src/collective/nitf/Extensions/Install.py
@@ -5,8 +5,9 @@ from Products.CMFCore.utils import getToolByName
 from collective.nitf.config import PROJECTNAME
 
 
-def uninstall(portal):
-    profile = 'profile-%s:uninstall' % PROJECTNAME
-    setup_tool = getToolByName(portal, 'portal_setup')
-    setup_tool.runAllImportStepsFromProfile(profile)
-    return "Ran all uninstall steps."
+def uninstall(portal, reinstall=False):
+    if not reinstall:
+        profile = 'profile-%s:uninstall' % PROJECTNAME
+        setup_tool = getToolByName(portal, 'portal_setup')
+        setup_tool.runAllImportStepsFromProfile(profile)
+        return "Ran all uninstall steps."
