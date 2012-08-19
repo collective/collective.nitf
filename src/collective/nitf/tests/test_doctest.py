85185e33ac88d84bc6cd7482bfab9d3b9b775343
hvelarde <hector.velarde@gmail.com>
2012-08-19 02:58:57 -0300

    skip functional tests (we must replace this with Robot Framework ones)

--- src/collective/nitf/tests/test_doctest.py
+++ src/collective/nitf/tests/test_doctest.py
@@ -7,11 +7,15 @@ from plone.testing import layered
 
 from collective.nitf.testing import FUNCTIONAL_TESTING
 
+# XXX: we must replace this tests with a Robot Framework ones
+optionflags = doctest.SKIP
+
 
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTests([
         layered(doctest.DocFileSuite('tests/functional.txt',
+                                     optionflags=optionflags,
                                      package='collective.nitf'),
                 layer=FUNCTIONAL_TESTING),
         ])
