c8854cc1ef627c67cd9372ee6ef5def92c497241
Érico Andrei <erico@simplesconsultoria.com.br>
2012-11-21 20:57:51 -0200

    PEP8

--- src/collective/nitf/controlpanel.py
+++ src/collective/nitf/controlpanel.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
-
-from zope import schema
-
-from z3c.form.browser.textlines import TextLinesFieldWidget
-from zope.interface import Interface
-
-from plone.app.registry.browser import controlpanel
-
 from collective.nitf import _
 from collective.nitf import config
+from plone.app.registry.browser import controlpanel
+from z3c.form.browser.textlines import TextLinesFieldWidget
+from zope import schema
+from zope.interface import Interface
+
+PORTALTYPES = 'plone.app.vocabularies.ReallyUserFriendlyTypes'
 
 
 class INITFSettings(Interface):
@@ -21,47 +19,47 @@ class INITFSettings(Interface):
         required=True,
         default=set(),
         value_type=schema.TextLine(title=_(u'Section')),
-        )
+    )
 
     default_section = schema.Choice(
         title=_(u"Default Section"),
         vocabulary=u'collective.nitf.AvailableSections',
         required=False,
-        )
+    )
 
     available_genres = schema.List(
         title=_(u"Available Genres"),
         description=_(u"Select the list of available genres in the site. "
-                       "Genres indicate a nature, journalistic or "
-                       "intellectual characteristic of items."),
+                      u"Genres indicate a nature, journalistic or "
+                      u"intellectual characteristic of items."),
         required=False,
         default=[],
         value_type=schema.Choice(vocabulary=u'collective.nitf.Genres'),
-        )
+    )
 
     default_genre = schema.Choice(
         title=_(u"Default Genre"),
         vocabulary=u'collective.nitf.Genres',
         required=False,
         default=config.DEFAULT_GENRE,
-        )
+    )
 
     default_urgency = schema.Choice(
         title=_(u"Default Urgency"),
         vocabulary=u'collective.nitf.Urgencies',
         required=False,
         default=config.DEFAULT_URGENCY,
-        )
+    )
 
     relatable_content_types = schema.List(
         title=_(u"Relatable Content Types"),
         description=_(u"Only objects of these content types will be "
-                       "listed in the related items widget."),
+                      u"listed in the related items widget."),
         required=False,
         default=config.DEFAULT_RELATABLE_CONTENT_TYPES,
         # we are going to list only the main content types in the widget
-        value_type=schema.Choice(vocabulary=u'plone.app.vocabularies.ReallyUserFriendlyTypes'),
-        )
+        value_type=schema.Choice(vocabulary=PORTALTYPES),
+    )
 
 
 class NITFSettingsEditForm(controlpanel.RegistryEditForm):
