6c5dac057cb1a680cab2986e8f422c3176b9a900
Juan Pablo Giménez <jpg@rcom.com.ar>
2013-02-12 18:52:00 -0300

    code clean up...

--- src/collective/nitf/static/nitf_fixes.js
+++ src/collective/nitf/static/nitf_fixes.js
@@ -52,22 +52,3 @@
 
 })(jQuery);
 
-function collapseSections() {
-  //we are in edit or add nitf content
-  if($("body").hasClass("template-collective.nitf.content")  || 
-    $("body").hasClass("portaltype-collective-nitf-content") && $("body").hasClass("template-edit")) {
-      $("fieldset").each(function(index) {
-       if(index != 0) {
-         $(this).collapse({ closed : true });
-       } else {
-         $(this).collapse();
-       }
-     });
-   } 
-}
-
-/*
-$(document).ready(function() {
-  collapseSections();
-});
-*/
