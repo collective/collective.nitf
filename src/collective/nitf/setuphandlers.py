7aff5e1c577d32e679c65a7a129383de18a39f52
Juan Pablo Giménez <jpg@rosario.com>
2013-09-12 16:43:26 -0300

    Fix upgrade step for Plone 4.2

--- src/collective/nitf/setuphandlers.py
+++ src/collective/nitf/setuphandlers.py
@@ -168,11 +168,9 @@ def upgrade_to_1008(context, logger=None):
 
     dependencies = ('plone.app.relationfield', 'plone.app.intid')
     for dependency in dependencies:
-        profile = 'profile-{0}:default'.format(dependency)
         qi = getToolByName(context, 'portal_quickinstaller')
         if not qi.isProductInstalled(dependency):
-            setup = getToolByName(context, 'portal_setup')
-            setup.runAllImportStepsFromProfile(profile)
+            qi.installProduct(dependency)
             logger.info("{0} was installed.".format(dependency))
         else:
             logger.info("{0} already installed; nothing to do.".format(dependency))
