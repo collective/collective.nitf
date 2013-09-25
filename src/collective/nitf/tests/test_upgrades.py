e018907b8d5adcdb8395a2680595788a721616a5
Marcos F. Romero <marcosfromero@gmail.com>
2013-09-25 15:50:14 -0300

    Test for upgrade step 1009

--- src/collective/nitf/tests/test_upgrades.py
+++ src/collective/nitf/tests/test_upgrades.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from collective.nitf.setuphandlers import upgrade_to_1008
+from collective.nitf.setuphandlers import upgrade_to_1009
 from collective.nitf.testing import INTEGRATION_TESTING
+from plone.app.relationfield.behavior import IRelatedItems
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from Products.CMFCore.utils import getToolByName
 from zope.component import queryUtility
 from zope.intid.interfaces import IIntIds
 
@@ -33,3 +38,38 @@ class Upgradeto1008TestCase(unittest.TestCase):
         for dependency in dependencies:
             self.assertTrue(qi.isProductInstalled(dependency), msg='{0} not installed'.format(dependency))
         self.assertIsNotNone(queryUtility(IIntIds))
+
+
+class Upgradeto1009TestCase(unittest.TestCase):
+
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+
+    def test_irelated_behavior(self):
+        """ Test the IRelatedItems behavior is installed by default.
+            Then removed, to simulate previous profile
+            And is added back once upgraded
+        """
+
+        # IRelatedItems is installed by default
+        ttool = getToolByName(self.portal, 'portal_types')
+        fti = ttool['collective.nitf.content']
+        self.assertTrue('plone.app.relationfield.behavior.IRelatedItems' in fti.behaviors)
+
+        # Remove behavior to simulate previous profile
+        behaviors = list(fti.behaviors)
+        behaviors.remove('plone.app.relationfield.behavior.IRelatedItems')
+        fti.behaviors = behaviors
+
+        # NITF doesn't provide IRelatedItems (as expected)
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.portal.invokeFactory('collective.nitf.content', 'n1')
+        n1 = self.portal['n1']
+        self.assertFalse(IRelatedItems.providedBy(n1))
+
+        # run the upgrade step and verify everything works as expected
+        upgrade_to_1009(self.portal)
+        self.assertTrue(IRelatedItems.providedBy(n1))
+        self.assertTrue('plone.app.relationfield.behavior.IRelatedItems' in fti.behaviors)
