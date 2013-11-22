a1474ec434dfb9e9addfefb356d46b81a5c3e331
hvelarde <hector.velarde@gmail.com>
2013-11-22 14:46:10 -0200

    Add upgrade step for removing character counter code
    - Unregister CSS and JS resources associated and cook them again
    - Remove control panel records from registry
    - Refactor test to make them easier to understand

--- src/collective/nitf/upgrades/v20/setuphandlers.py
+++ src/collective/nitf/upgrades/v20/setuphandlers.py
@@ -7,15 +7,27 @@ import logging
 
 
 def miscellaneous(context, logger=None):
-    """Apply upgrade profile."""
+    """Apply upgrade profile; this includes:
+
+    - remove character counter resources from CSS and JS registries
+    - remove character counter control panel records from registry
+    - rename gallery view in News Article content type
+    - cook CSS and JS resources
+    """
     if logger is None:
         # Called as upgrade step: define our own logger
         logger = logging.getLogger(PROJECTNAME)
 
-    # rename view on content type registry
     profile = 'profile-collective.nitf.upgrades.v20:to20alpha1'
     setup = api.portal.get_tool('portal_setup')
     setup.runAllImportStepsFromProfile(profile)
+
+    portal_css = api.portal.get_tool('portal_css')
+    portal_css.cookResources()
+    logger.info(u'CSS resources cooked')
+    portal_js = api.portal.get_tool('portal_javascripts')
+    portal_js.cookResources()
+    logger.info(u'JS resources cooked')
 
 
 def update_layouts(context, logger=None):
@@ -24,13 +36,14 @@ def update_layouts(context, logger=None):
         logger = logging.getLogger(PROJECTNAME)
 
     # update existing objects
-    logger.info(u'Updating News Articles layout')
+    logger.info(u'Updating layout of News Articles')
     catalog = api.portal.get_tool('portal_catalog')
     results = catalog(portal_type='collective.nitf.content')
+    logger.info(u'{0} News Articles found'.format(len(results)))
     i = 0
     for item in results:
         obj = item.getObject()
         if obj.getLayout() == 'nitf_galleria':
             obj.setLayout('galleria')
             i += 1
-    logger.info(u'{0} News Articles updated'.format(i))
+        logger.info(u'{0} News Articles updated'.format(i))
