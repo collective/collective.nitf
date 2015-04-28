e24e6c3d8fa3dbdb9b684cbdbc8e25c02ec34c69
hvelarde <hector.velarde@gmail.com>
2015-04-28 14:23:47 -0300

    Set development status as Alpha

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(name='collective.nitf',
       description=description,
       long_description=long_description,
       classifiers=[
-          'Development Status :: 4 - Beta',
+          'Development Status :: 3 - Alpha',
           'Environment :: Web Environment',
           'Framework :: Plone',
           'Framework :: Plone :: 4.3',
