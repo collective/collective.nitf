19786e6a103a24e0889718611c5ff797f8bae066
hvelarde <hector.velarde@gmail.com>
2015-04-28 16:01:32 -0300

    Refactor upgrade step to 1007

--- src/collective/nitf/upgrades/v1007/__init__.py
+++ src/collective/nitf/upgrades/v1007/__init__.py
@@ -1,13 +1,17 @@
 # -*- coding:utf-8 -*-
 from collective.nitf.config import PROJECTNAME
-from plone.app.upgrade.utils import loadMigrationProfile
+from plone import api
 
 import logging
+logger = logging.getLogger(PROJECTNAME)
 
 
-def apply_profile(context):
-    """Update to 1007 version."""
-    logger = logging.getLogger(PROJECTNAME)
-    profile = 'profile-collective.nitf.upgrades.v1007:default'
-    loadMigrationProfile(context, profile)
-    logger.info('Updated to version 1007')
+def add_locking_behaviour(context):
+    """Add locking behaviour to News Article."""
+    locking_behavior = 'plone.app.lockingbehavior.behaviors.ILocking'
+    types = api.portal.get_tool('portal_types')
+    nitf = types['collective.nitf.content']
+    behaviors = list(nitf.behaviors)
+    behaviors.append(locking_behavior)
+    nitf.behaviors = tuple(behaviors)
+    logger.info('ILocking behavior added to News Article content type.')
