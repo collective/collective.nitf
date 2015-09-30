f485098692241d7a9c2576be602220ddbfb7e8f9
hvelarde <hector.velarde@gmail.com>
2015-09-30 16:52:07 -0300

    Back to development: 2.0b1

--- CHANGES.rst
+++ CHANGES.rst
@@ -3,6 +3,12 @@ Changelog
 
 There's a frood who really knows where his towel is.
 
+2.0b1 (unreleased)
+^^^^^^^^^^^^^^^^^^
+
+- Nothing changed yet.
+
+
 2.0a1 (2015-09-30)
 ^^^^^^^^^^^^^^^^^^
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '2.0a1'
+version = '2.0b1.dev0'
 description = "A content type inspired on the IPTC's News Industry Text Format specification."
 long_description = (
     open('README.rst').read() + '\n' +
