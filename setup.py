8359d581a63f1f77b3bb24ff176e9d6c4b936640
idgserpro <idg@serpro.gov.br>
2021-04-15 11:32:53 -0300

    Back to development: 2.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = '2.1.1'
+version = '2.1.2.dev0'
 description = "A content type inspired on the IPTC's News Industry Text Format specification."
 long_description = (
     open('README.rst').read() + '\n' +
