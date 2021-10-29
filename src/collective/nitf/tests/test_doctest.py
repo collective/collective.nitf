551276b9854cb40e8a8b05518ac8b5503995d213
wesleybl <wesleybl@gmail.com>
2021-10-29 14:51:53 -0300

    Fix test_collection.txt in Plone 5.2
    Collection criteria are now returned by ajax via @@qsOptions view

--- src/collective/nitf/tests/test_doctest.py
+++ src/collective/nitf/tests/test_doctest.py
@@ -13,10 +13,6 @@ files = os.listdir(dirname)
 tests = [f for f in files if f.startswith("test_") and f.endswith(".txt")]
 
 
-# FIXME: Fix test_collection.txt in Plone 5.2
-tests = []
-
-
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTests(
