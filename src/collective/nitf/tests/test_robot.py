c7617b2b54af6fca3518a68b69b9a940caaef110
wesleybl <wesleybl@gmail.com>
2021-12-06 12:10:16 -0300

    Fix test_content.robot

--- src/collective/nitf/tests/test_robot.py
+++ src/collective/nitf/tests/test_robot.py
@@ -10,20 +10,13 @@ import unittest
 files = os.listdir(os.path.dirname(__file__))
 tests = [f for f in files if f.startswith("test_") and f.endswith(".robot")]
 
-noncritical = ["Expected Failure"]
-# FIXME: https://github.com/collective/collective.nitf/issues/172
-noncritical.append("issue_172")
-
-# FIXME: Make RobotFramework tests work in Plone 5
-tests = ["test_views.robot"]
-
 
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTests(
         [
             layered(
-                robotsuite.RobotTestSuite(t, noncritical=noncritical),
+                robotsuite.RobotTestSuite(t),
                 layer=ROBOT_TESTING,
             )
             for t in tests
